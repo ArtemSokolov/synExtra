1c5429a612046bb9ec5dcda04df1d624da5fb4cf
Artem Sokolov <artem.sokolov@gmail.com>
2018-08-31 13:40:55 -0400

    Implemented synAncestry()

--- R/syn.R
+++ R/syn.R
@@ -30,9 +30,11 @@ isSynID <- function( ids )
 #' @param ... Additional arguments to be passed to synapser::synGet()
 #' @return A downloader function that recognizes synapse IDs and downloads the associated files.
 #' @examples
+#' \dontrun{
 #' syn <- synDownloader( "/data", ifcollision="overwrite.local" )
 #' syn( "syn15663039", "localFile.csv" )
 #' # [1] "/data/mtcars.csv" "localFile.csv"
+#' }
 #' @importFrom magrittr %>%
 #' @export
 synDownloader <- function( dloc, ... )
@@ -46,3 +48,36 @@ synDownloader <- function( dloc, ... )
     { purrr::flatten(list(...)) %>% purrr::map_if( isSynID, dl ) %>% purrr::flatten_chr() }
 }
 
+#' Retrieve parentId chain
+#'
+#' Recursively reconstructs parentId ancestry for a given Synapse ID
+#'
+#' @param ... One or more Synapse IDs for which ancestry is to be reconstructed
+#' @return For each requested ID, the function returns a vector of parentId ancestry.
+#' The first entry in each vector is the query ID itself; the last entry is the project.
+#' @examples
+#' \dontrun{
+#' synAncestry( "syn15663039", "syn1695362" )
+#' # [[1]]
+#' # [1] "syn15663039" "syn15673834" "syn15673837" "syn12180284"
+#' #
+#' # [[2]]
+#' # [1] "syn1695362" "syn1695324" "syn2812925" "syn300013" 
+#' }
+#' @export
+synAncestry <- function(...)
+{
+    ## Split up computation, if multiple IDs are provided
+    l <- purrr::flatten(list(...))
+    if( length(l) > 1 ) return( purrr::map(l, synAncestry) )
+    id <- unlist(l)	## Dealing with a single id
+    
+    ## Retrieve the entity
+    s <- synapser::synGet( id, downloadFile=FALSE )
+
+    ## Terminate recursion at the Project level
+    if( grepl( "Project", s$properties$entityType ) ) return(id)
+
+    ## Recurse up the chain
+    c( id, synAncestry( s$properties$parentId ) )
+}
