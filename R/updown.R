cc27ff88009bd1ceb81778274d7f3f5909a8dbfa
Clemens Hug <clemens.hug@gmail.com>
2019-08-29 12:43:14 -0400

    Improve synStoreMany
    * Can deal with vectorized input
    * Accepts Activity object for storing file provenance

--- R/updown.R
+++ R/updown.R
@@ -10,7 +10,7 @@
 #' Any recognized Synapse IDs get downloaded to the local folder specified by dloc.
 #' The downloader then returns local paths to these files. Strings not recognized to be
 #' valid Synapse IDs are returned as is.
-#' 
+#'
 #' @param dloc Path to a local folder where downloaded files will be stored
 #' @param ... Additional arguments to be passed to synapser::synGet()
 #' @return A downloader function that recognizes synapse IDs and downloads the associated files.
@@ -34,25 +34,28 @@ synDownloader <- function( dloc, ... )
 }
 
 #' Upload a directory structure to Synapse
-#' 
+#'
 #' Recursively uploads a directory and all of its content to the given Synapse project/folder
 #'
-#' @param localPath A character string to the local file / directory to upload
+#' @param localPath A character string or vector of strings to the local file / directory to upload
 #' @param parentId A synapse ID of the hosting project or parent folder on Synapse
+#' @param activity An Activity metadata object, see \link[synapser]{Activity}
 #' @importFrom magrittr %>%
 #' @return A nested list capturing the created hierarchy of Synapse IDs
 #' @export
-synStoreMany <- function( localPath, parentId )
+synStoreMany <- function( localPath, parentId, activity = NULL )
 {
+    if ( length(localPath) > 1 ) return( purrr::map( localPath, synStoreMany, parentId, activity ) )
+
     ## Case 1: localPath is a directory
     if( dir.exists(localPath) )
     {
         ## Special case: current directory
-        if( localPath == "." ) return(synStoreMany( getwd(), parentId ))
+        if( localPath == "." ) return(synStoreMany( getwd(), parentId, activity ))
 
         ## Special case: parent directory
-        if( localPath == ".." ) return(synStoreMany( dirname(getwd()), parentId ))
-        
+        if( localPath == ".." ) return(synStoreMany( dirname(getwd()), parentId, activity ))
+
         ## Replicate a folder to Synapse
         cat( "Creating", basename(localPath), "\n" )
         f <- synapser::Folder( name = basename(localPath), parentId = parentId ) %>% synapser::synStore()
@@ -60,13 +63,13 @@ synStoreMany <- function( localPath, parentId )
 
         ## Recurse onto files in the directory
         res <- list.files( localPath, include.dirs=TRUE, full.names=TRUE ) %>%
-            purrr::map( synStoreMany, pid ) %>% purrr::flatten() %>% list() %>% setNames( pid )
+            purrr::map( synStoreMany, pid, activity=activity ) %>% purrr::flatten() %>% list() %>% setNames( pid )
     }
-    
+
     ## Case 2: localPath is a file; simply upload it
     else
     {
-        f <- synapser::File( path=localPath, parent=parentId ) %>% synapser::synStore()
+        f <- synapser::File( path=localPath, parent=parentId ) %>% synapser::synStore(activity=activity)
         cat( "\n" )
         res <- list(localPath) %>% setNames( f$properties$id )
     }
